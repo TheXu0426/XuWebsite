5d03dcc36ac994e2801fad0f707a37edc4a2748d
TheXu0426 <137465316+TheXu0426@users.noreply.github.com>
2023-11-28 23:29:01 +0800

    Update certificate.js (phone interaction UI)
    phone platform interaction UI

--- certificate.js
+++ certificate.js
@@ -2,11 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const mainImage = document.getElementById('main-image');
     const overlay = document.getElementById('overlay');
 
-    mainImage.addEventListener('mouseenter', function () {
+    function showOverlay() {
         overlay.style.opacity = 1;
-    });
+    }
 
-    overlay.addEventListener('mouseleave', function () {
+    function hideOverlay() {
         overlay.style.opacity = 0;
-    });
+    }
+
+    // Mouse events
+    mainImage.addEventListener('mouseenter', showOverlay);
+    overlay.addEventListener('mouseleave', hideOverlay);
+
+    // Touch events
+    mainImage.addEventListener('touchstart', showOverlay);
+    overlay.addEventListener('touchend', hideOverlay);
 });
